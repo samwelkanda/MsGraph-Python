d8bfeb1123c5dbf493fd2570bb2748b1f4ded18f
Sam Kiptoo <samwelkanda@gmail.com>
2021-01-13 14:00:01 +0300

    Register the app and add azure ad

--- graph_tutorial/tutorial/urls.py
+++ graph_tutorial/tutorial/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
   # /
   path('', views.home, name='home'),
-  # TEMPORARY
-  path('signin', views.home, name='signin'),
-  path('signout', views.home, name='signout'),
+  path('signin', views.sign_in, name='signin'),
+  path('callback', views.callback, name='callback'),
+  path('signout', views.sign_out, name='signout'),
   path('calendar', views.home, name='calendar'),
 ]

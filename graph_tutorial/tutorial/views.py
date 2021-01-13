16904a3ff0894a5df6e7067c52ae19526e9941c0
Sam Kiptoo <samwelkanda@gmail.com>
2021-01-13 12:00:00 +0300

    Initial application setup

---
+++ graph_tutorial/tutorial/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from datetime import datetime, timedelta
+
+def home(request):
+  # Temporary!
+  return HttpResponse("Welcome to the tutorial.")
+

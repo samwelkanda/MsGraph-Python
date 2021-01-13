abd3ef9150089042d40238002510c8230b2089b0
Sam Kiptoo <samwelkanda@gmail.com>
2021-01-13 14:19:34 +0300

    Get a calendar view

--- graph_tutorial/tutorial/views.py
+++ graph_tutorial/tutorial/views.py
@@ -53,3 +53,48 @@ def sign_out(request):
   remove_user_and_token(request)
 
   return HttpResponseRedirect(reverse('home'))
+
+def calendar(request):
+  context = initialize_context(request)
+  user = context['user']
+
+  # Load the user's time zone
+  # Microsoft Graph can return the user's time zone as either
+  # a Windows time zone name or an IANA time zone identifier
+  # Python datetime requires IANA, so convert Windows to IANA
+  time_zone = get_iana_from_windows(user['timeZone'])
+  tz_info = tz.gettz(time_zone)
+
+  # Get midnight today in user's time zone
+  today = datetime.now(tz_info).replace(
+    hour=0,
+    minute=0,
+    second=0,
+    microsecond=0)
+
+  # Based on today, get the start of the week (Sunday)
+  if (today.weekday() != 6):
+    start = today - timedelta(days=today.isoweekday())
+  else:
+    start = today
+
+  end = start + timedelta(days=7)
+
+  token = get_token(request)
+
+  events = get_calendar_events(
+    token,
+    start.isoformat(timespec='seconds'),
+    end.isoformat(timespec='seconds'),
+    user['timeZone'])
+
+  if events:
+    # Convert the ISO 8601 date times to a datetime object
+    # This allows the Django template to format the value nicely
+    for event in events['value']:
+      event['start']['dateTime'] = parser.parse(event['start']['dateTime'])
+      event['end']['dateTime'] = parser.parse(event['end']['dateTime'])
+
+    context['events'] = events['value']
+
+  return render(request, 'tutorial/calendar.html', context)
